7cf6daf559a1504414db2aa1f0d99fb341dd9713
Moisés Berenguer <moises.berenguer@gmail.com>
2024-11-15 20:29:57 -0400

    Update config.sh

--- config.sh
+++ config.sh
@@ -94,7 +94,7 @@ export PYENV_ROOT="\$HOME/.pyenv"
 eval "\$(pyenv init -)"
 
 # Pyenv virtualenv configuration
-eval "$(pyenv virtualenv-init -)"
+eval "\$(pyenv virtualenv-init -)"
 
 EOT
 . $PYENV_CONFIG
